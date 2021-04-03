fa2bcefda34554e1919abf8363a20ecc0a045e69
Alisson Sampaio de Carvalho Alencar <alencar.alisson.sc@gmail.com>
2021-04-03 11:49:51 -0300

    monte carlo sampler

--- run_monte_carlo_sampler.py
+++ run_monte_carlo_sampler.py
@@ -12,14 +12,18 @@ methods			= ['mlat', 'lin', 'pmlat', 'pml']
 
 
 for method in methods:
+	print(method)
 	for k in Ks:
+		print('\tK = %d'%(k))
 		for radius in Radius:
+			print('\t\tradius = %d'%(radius))
 			dataset = pickle.load(open('./dataset/nl_0_K_'+str(k)+'_RD_'+str(radius)+'.pic', 'rb'))[0]
 			for noise_level in Noise_level:
+				print('\t\t\tnoise level = %d'%(noise_level))
+				
 				R = dataset['R']
 				d = dataset['d']
 				# q = dataset['q']
 
 				Q = mlat.monte_carlo_sampler(method, R, d, noise_level)
-				pickle.dump(err, open('results/samples/MT_'+method+'_K_'+str(k)+'_RD_'+str(rd)+'_nl_'+str(noise_level)+'.pic', 'wb'))
-				
+				pickle.dump(Q, open('results/samples/MT_'+method+'_K_'+str(k)+'_RD_'+str(rd)+'_nl_'+str(noise_level)+'.pic', 'wb'))
