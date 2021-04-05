20811fc0afa793fa4059c5f8d427b5ef92712b84
Alisson Sampaio de Carvalho Alencar <alencar.alisson.sc@gmail.com>
2021-04-05 15:09:36 -0300

    30 runs likelihood on montecarlo sampler methods

--- loglikelihood.py
+++ loglikelihood.py
@@ -2,55 +2,50 @@ import numpy as np
 from scipy.stats import multivariate_normal
 import multilateration as mlat
 from scipy.spatial import distance
+import pandas as pd
 import pickle
 
 
-noise_level = 10
-n_samples   = 1000
-K           = 3
-n_runs      = 30
 
+Radius       	= [10, 20, 30] 		# distance between the reference points and the query point
+Noise_level 	= 1+np.arange(10)	# noise level
+Ks          	= [3, 6, 10]		# number of reference points
+methods			= ['pmlat', 'lin', 'mlat', 'pml']#, 'BMlat']
+n_runs			= 30				# number of runs
 
-dataset = pickle.load(open('./dataset/04_03/likelihood/nl_0_K_3_RD_30.pic', 'rb'))
+for k in Ks:
+	print('K = %d'%(k))
+	for radius in Radius:
+		print('\tradius = %d'%(radius))
 
-err = np.zeros([len(dataset), 1])
-for i, data in enumerate(dataset):
-	R = data['R']
-	d = data['d']
-	q = data['q']
+		q_star = pickle.load(open('./dataset/nl_0_K_'+str(k)+'_RD_'+str(radius)+'.pic', 'rb'))[0]['q']
 
-	# Q_lin  = np.zeros([n_samples, 2])
-	# Q_reg  = np.zeros([n_samples, 2])
-	# Q_prob = np.zeros([n_samples, 2])
-	Q_pml  = np.zeros([n_samples, 2])
-	for j in np.arange(n_samples):
-		print('\tsample %d'%(j))
-		R2 = R + np.random.normal(0, noise_level,size=[K,2])
-		# Q_lin[j,:]  = mlat.lin_multilateration(R2, d)
-		# Q_reg[j,:]  = mlat.multilateration(R2, d)
-		# Q_prob[j,:] = mlat.dj_multilateration(R2, d)
-		Q_pml[j,:]  = mlat.PML(R2, d)
+		loglike = {}
+		n = []
+		l = []
+		m = []
+		r = []
+		for method in methods:
+			print('\t\t'+method)		
 
+			for noise_level in Noise_level:
+				print('\t\t\tnoise level = %d'%(noise_level))
 
-	# model = mlat.ProbabilisticMultilateration()
-	# model.sampling(R, d, sigma=noise_level, theta=100, max_treedepth=12, adapt_delta=0.9)
+				for run in np.arange(n_runs):
+					Q = pickle.load(open('results/samples/MT_'+method+'_K_'+str(k)+'_RD_'+str(radius)+'_nl_'+str(noise_level)+'_r_'+str(run)+'.pic', 'rb'))
+					
+					mu= np.mean(Q, axis=0)
+					C = np.cov(Q.T)
+					n.append(noise_level)
+					l.append(multivariate_normal.pdf(q_star, mu, C))
+					m.append(method)
+					r.append(run)
+							
+		loglike['noise_level'] 	= n
+		loglike['likelihood']  	= l
+		loglike['method']		= m
+		loglike['run']			= r
 
-	# m = np.mean(Q_lin, axis=0)
-	# C = np.cov(Q_lin.T)
-	# err[i,0] = multivariate_normal.pdf(q, m, C)
-	# m = np.mean(Q_reg, axis=0)
-	# C = np.cov(Q_reg.T)
-	# err[i,1] = multivariate_normal.pdf(q, m, C)
-	# m = np.mean(Q_prob, axis=0)
-	# C = np.cov(Q_prob.T)
-	# err[i,2] = multivariate_normal.pdf(q, m, C)
-	m = np.mean(Q_pml, axis=0)
-	C = np.cov(Q_pml.T)
-	err[i,0] = multivariate_normal.pdf(q, m, C)
-	print('loglikelihood %d: %1.4f'%(i, err[i,0]))
-	# m = np.mean(model.Q, axis=0)
-	# C = np.cov(model.Q.T)
-	# err[i,3] = multivariate_normal.pdf(q, m, C)
-pickle.dump(err, open('results/25_03/likelihood/err_circle.pic', 'wb'))
-print(err)
-print(err.mean(axis=0))
+		df = pd.DataFrame(loglike)
+		df.to_csv('./results/loglikelihood/loglike'+'_K_'+str(k)+'_RD_'+str(radius)+'.csv')
+		# pickle.dump(loglike, open('./results/loglikelihood/loglike'+'_K_'+str(k)+'_RD_'+str(radius)+'.pic', 'wb'))
