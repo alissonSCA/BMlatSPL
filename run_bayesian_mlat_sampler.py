3d24d2b5bc69a9753cf18a0f3991f830d3d8dd79
Alisson Sampaio de Carvalho Alencar <alencar.alisson.sc@gmail.com>
2021-04-05 15:25:03 -0300

    30 runs on bayesian sampler

--- run_bayesian_mlat_sampler.py
+++ run_bayesian_mlat_sampler.py
@@ -6,19 +6,21 @@ import pickle
 Radius       	= [10, 20, 30] 		# distance between the reference points and the query point
 Noise_level 	= 1+np.arange(10)	# noise level
 Ks          	= [3, 6, 10]		# number of reference points
+n_runs			= 30				# number of runs
 
-model = mlat.ProbabilisticMultilateration()
+model = mlat.BayesianMultilateration()
 for k in Ks:
 	print('\tK = %d'%(k))
 	for radius in Radius:
 		print('\t\tradius = %d'%(radius))
 		dataset = pickle.load(open('./dataset/nl_0_K_'+str(k)+'_RD_'+str(radius)+'.pic', 'rb'))[0]
+		R = dataset['R']
+		d = dataset['d']
 		for noise_level in Noise_level:
 			print('\t\t\tnoise level = %d'%(noise_level))
 
-			R = dataset['R']
-			d = dataset['d']
-			# q = dataset['q']
+			for r in np.arange(n_runs):
+				print('\t\t\trun = %d'%(r))
 
-			model.sampling(R, d, sigma=noise_level, theta=100, max_treedepth=12, adapt_delta=0.9)
-			pickle.dump(model.Q, open('results/samples/MT_BMlat_K_'+str(k)+'_RD_'+str(radius)+'_nl_'+str(noise_level)+'.pic', 'wb'))
+				model.sampling(R, d, sigma=noise_level, theta=100, max_treedepth=12, adapt_delta=0.9)
+				pickle.dump(model.Q, open('results/samples/MT_BMlat_K_'+str(k)+'_RD_'+str(radius)+'_nl_'+str(noise_level)+'_r_'+str(r)+'.pic', 'wb'))
